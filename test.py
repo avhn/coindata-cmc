f2ea3248f3de71e83ccac3d25fbaae0334493446
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2018-12-29 14:57:57 +0300

    Update unittest at test.py

--- test.py
+++ test.py
@@ -2,33 +2,32 @@ import unittest
 import os
 import coindata
 
-_possible_input_formats = ('btc', 'BTC', 'bitcoin', 'BITCOIN')
-
-
-def is_same(data1, data2):
-    """Compare 2 coindata outputs."""
-
-    # length case
-    if not len(data1) == len(data2):
-        return False
-
-    # comparing keys
-    for i in range(len(data1)):
-        for key in data1[i]:
-            if not data1[i][key] == data2[i][key]:
-                return False
-    
-    # passed
-    return True
-
 
 class TestBuild(unittest.TestCase):
-    
-    def test_retrieve(self):
-        """Try retrieve with all possible inputs."""
+    _possible_input_formats = ('btc', 'BTC', 'bitcoin', 'BITCOIN')
 
-        for inp in _possible_input_formats:
-            output = coindata.retrieve(inp)
+    @staticmethod
+    def is_same(data1, data2):
+        """Compare 2 coindata outputs."""
+
+        # length case
+        if not len(data1) == len(data2):
+            return False
+
+        # comparing keys
+        for i in range(len(data1)):
+            for key in data1[i]:
+                if not data1[i][key] == data2[i][key]:
+                    return False
+    
+                # passed
+                return True
+
+    def test_retrieve(self):
+        """Try retrieve with all possible input formats."""
+
+        for inp in self._possible_input_formats:
+            output = coindata.request.retrieve(inp)
 
             if not output:
                 raise ValueError()
@@ -37,18 +36,18 @@ class TestBuild(unittest.TestCase):
         """Compare retrieve and read outputs."""
         
         # retrieve
-        data = coindata.retrieve('btc')
+        data = coindata.request.retrieve('btc')
 
         # write and read
         dir_test = os.path.dirname(os.path.realpath(__file__))
-        file_path = coindata.write('btc', dir_test)
-        data_comp = coindata.read(file_path)
+        file_path = coindata.request.write('btc', dir_test)
+        data_comp = coindata.request.read(file_path)
         
         # clean up
         os.remove(file_path)
 
         # test
-        if not is_same(data, data_comp):
+        if not self.is_same(data, data_comp):
             raise ValueError()
 
 
