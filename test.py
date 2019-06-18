dbde4f480074de19051f1539d9e8a4057cda598d
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-06-18 13:53:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import unittest
 import os
 import coindata
 
 
-class TestBuild(unittest.TestCase):
-    _possible_input_formats = ('btc', 'BTC', 'bitcoin', 'BITCOIN')
+class TestBasicOperations(unittest.TestCase):
+    possible_input_formats = ('btc', 'BTC', 'bitcoin', 'BITCOIN')
 
     @staticmethod
     def is_same(data1, data2):
@@ -20,43 +23,45 @@ class TestBuild(unittest.TestCase):
                 if not data1[i][key] == data2[i][key]:
                     return False
     
-                # passed
-                return True
+            # passed
+            return True
 
     def test_retrieve_with_all_input_formats(self):
         """Try retrieve with all possible input formats."""
 
-        for inp in self._possible_input_formats:
+        for inp in self.possible_input_formats:
             output = coindata.request.retrieve(inp)
 
             if not output:
-                raise ValueError()
+                raise ValueError("No output with following input: " + inp)
             
     def test_write_read(self):
-        """Compare retrieve and read outputs."""
+        """Compare request.retrieve and request.read outputs."""
         
         # retrieve
         data = coindata.request.retrieve('btc')
-
         # write and read
         dir_test = os.path.dirname(os.path.realpath(__file__))
         file_path = coindata.request.write('btc', dir_test)
         data_comp = coindata.request.read(file_path)
-        
         # clean up
         os.remove(file_path)
 
         # test
         if not self.is_same(data, data_comp):
-            raise ValueError()
+            raise ValueError("Written and read data aren't same!")
 
     def test_cache_and_get(self):
-        """Compare cache and get's outputs."""
+        """Test basic caching and get."""
 
-        coindata.cache('BITCOIN')
-        coindata.get('btc')
-
+        cache = coindata.cache('BITCOIN')
+        get = coindata.get('btc')
+        
 
 if __name__ == "__main__":
-    unittest.main()
-
+    
+    try:
+        unittest.main()
+        
+    except ConnectionError:
+        print("No internet connection, aborting!")
