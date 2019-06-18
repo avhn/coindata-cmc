1b197b21bb2dd1359d1c35c3958592f2a2dcd102
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-06-18 13:57:53 +0300

    Fix import order at __init__.py

--- coindata/__init__.py
+++ coindata/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .cacher import cache
 from .parser import vector_of as get
+from .cacher import cache
 from .request import ISO8601
 from . import utils
